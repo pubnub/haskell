fd29d870922ced61a5b9226419b63545efd78291
Tristan Sloughter <t@crashfast.com>
2014-01-05 16:20:48 -0600

    fix resume on reconnect logic

--- src/Network/Pubnub.hs
+++ src/Network/Pubnub.hs
@@ -54,17 +54,19 @@ subscribe pn subOpts uid =
   async (withManager $ \manager -> connect pn{time_token=(Timestamp 0)} manager False)
   where
     connect pn' manager isReconnect = do
-      let req = buildSubscribeRequest pn'
+      let req = buildSubscribeRequest pn' "0"
       res <- httpLbs req manager
       case decode $ responseBody res of
         Just (ConnectResponse ([], t)) -> do
           if isReconnect then liftIO (onConnect subOpts) else liftIO (onReconnect subOpts)
-          subscribe' manager pn{time_token=t}
+          if (resumeOnReconnect subOpts && isReconnect)
+            then subscribe' manager pn
+            else subscribe' manager pn{time_token=t}
         _ ->
           subscribe' manager pn
 
     subscribe' manager pn' = do
-      let req = buildSubscribeRequest pn'
+      let req = buildSubscribeRequest pn' $ head . L.toChunks $ encode (time_token pn')
       eres <- try $ httpLbs req manager
       case eres of
         Right r ->
@@ -94,17 +96,14 @@ subscribe pn subOpts uid =
           liftIO (onDisconnect subOpts)
           reconnect pn' manager
 
-    reconnect pn' manager = do
-      if (resumeOnReconnect subOpts)
-        then connect pn' manager True
-        else connect pn'{time_token=(Timestamp 0)} manager True
+    reconnect pn' manager = connect pn' manager True
 
-    buildSubscribeRequest pn' =
+    buildSubscribeRequest pn' tt =
       buildRequest pn' [ "subscribe"
                        , encodeUtf8 $ sub_key pn'
                        , encodeUtf8 $ T.intercalate "," (channels pn')
                        , bsFromInteger $ jsonp_callback pn'
-                       , head . L.toChunks $ encode (time_token pn')]
+                       , tt ]
       (case uid of
           Just u -> [("uuid", encodeUtf8 u)]
           Nothing -> [])
